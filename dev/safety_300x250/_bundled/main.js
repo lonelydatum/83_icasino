327bf82413c4e65a04d76e2bbd13417d91b1143e
Gar <gar@lonelydatum.com>
2024-10-28 14:07:24 -0400

    on

--- dev/safety_300x250/_bundled/main.js
+++ dev/safety_300x250/_bundled/main.js
@@ -80,7 +80,7 @@ exports.olg_ypy = olg_ypy;
 exports.rotateScale1 = rotateScale1;
 exports.rotateScale2 = rotateScale2;
 
-},{"./helpers/helpers.js":2,"./proline":3,"./ypy_fx.js":4}],2:[function(require,module,exports){
+},{"./helpers/helpers.js":2,"./proline":4,"./ypy_fx.js":5}],2:[function(require,module,exports){
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -93,6 +93,76 @@ function origin(el, x, y) {
 exports.origin = origin;
 
 },{}],3:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _commonJs = require('./common.js');
+
+document.getElementById("legalContent").innerHTML = "\n©2024 IGT.<br/><br/>\nMust be 19 years of age or older and a resident \nof Ontario, located in the province to play online\ncasino games. Games may not appear as shown. Odds vary by game. Terms and conditions apply.\n<br/>\n*Voted most trusted Online Casino by Ontario shoppers based on the 2023 Brandspark® \n<br/>\nCanadian Trust Study.";
+
+var READ = {
+	t1: 2.2,
+	t2: 2
+};
+
+function startBasic(_ref) {
+	var shiftY = _ref.shiftY;
+
+	var tl = (0, _commonJs.init)();
+	var rotate = 150;
+	tl.add("arcs-in");
+	TweenLite.set(".ring", { opacity: 1 });
+	var ring = { opacity: 0, ease: "power2.out", duration: .4 };
+	tl.from(".ring1_1", _extends({}, ring, { rotate: -rotate }), "arcs-in+=0");
+	tl.from(".ring1_2", _extends({}, ring, { rotate: rotate }), "arcs-in+=.1");
+	tl.from(".ring1_3", _extends({}, ring, { rotate: -rotate }), "arcs-in+=.2");
+	tl.from(".ring1_4", _extends({}, ring, { rotate: rotate }), "arcs-in+=.3");
+
+	tl.from(".ypy1-1", { opacity: 0, rotate: -60, duration: .5 }, "arcs-in+=0.2");
+	tl.from(".ypy1-2", { opacity: 0, rotate: -60, duration: .5 }, "arcs-in+=.5");
+	tl.from(".ypy1-3", { opacity: 0, rotate: -60, duration: .5 }, "arcs-in+=.7");
+
+	tl.add("shrink", "+=.3");
+
+	tl.to(".ring", { opacity: 0, duration: .3 }, "shrink");
+	tl.from(".ring_all", { opacity: 0, duration: .3 }, "shrink");
+
+	tl.to(".hero", { x: 0, y: 0, scale: .50, duration: .3 }, "shrink");
+	tl.from(".footer-bar", { y: _commonJs.bannerSize.h, duration: .3 }, "shrink");
+	tl.from(".t1", { y: 250, duration: .4 }, "shrink");
+	tl.to(".ypy1-1", { x: -133, y: -178, duration: .3 }, "shrink");
+	tl.to(".ypy1-2", { x: -173, y: -172, duration: .3 }, "shrink");
+	tl.to(".ypy1-3", { x: -140, y: -191, duration: .3 }, "shrink");
+
+	tl.to(".ring", { y: "-=" + 30, duration: .4 }, "shrink");
+
+	tl.from([".inset"], { y: "+=" + 30, opacity: 0, duration: .4 }, "shrink+=.2");
+	tl.from([".brand-logo"], { opacity: 0, duration: .4 }, "shrink+=.2");
+
+	tl.to(".t1", { opacity: 0, duration: .3 }, "+=" + READ.t1);
+	tl.from([".t2", ".brand-logo"], { opacity: 0, duration: .3 });
+
+	tl.add("end", "+=" + READ.t2);
+	tl.to(".frame1", { y: "-=" + _commonJs.bannerSize.h, duration: .5 }, "end");
+	tl.set(".frame2", { opacity: 1 }, "end");
+	tl.from(".frame2", { y: "+=" + _commonJs.bannerSize.h, duration: .5 }, "end");
+
+	tl.from(".url", { opacity: 0, duration: .3 });
+
+	tl.add((0, _commonJs.olg_ypy)(), "-=.3");
+
+	return tl;
+}
+
+exports.startBasic = startBasic;
+exports.READ = READ;
+
+},{"./common.js":1}],4:[function(require,module,exports){
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -118,7 +188,7 @@ function olg() {
 
 exports.olg = olg;
 
-},{}],4:[function(require,module,exports){
+},{}],5:[function(require,module,exports){
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -151,74 +221,27 @@ initYPY();
 exports.initYPY = initYPY;
 exports.ypyScroll = ypyScroll;
 
-},{}],5:[function(require,module,exports){
-// import { READ, startBasic} from '../../_common/js/player_support.js'
-"use strict";
+},{}],6:[function(require,module,exports){
+'use strict';
 
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+var _commonJsPlayer_supportJs = require('../../_common/js/player_support.js');
 
 var _commonJsCommonJs = require('../../_common/js/common.js');
 
-// start()
+start();
 function start() {
 
-	// 349 50
-	var data = { list: [".ring1_1", ".ring1_2", ".ring1_3"],
-		offsetX: 146, offsetY: 190, to: { x: 160, y: 190 } };
+	// // 349 50
+	// const data = {list:[".ring1_1", ".ring1_2", ".ring1_3"],
+	// offsetX:146, offsetY:190, to:{x:160, y:190}}
 
-	(0, _commonJsCommonJs.rotateScale1)(data);
-	(0, _commonJsCommonJs.rotateScale2)({ list: [".ypy1-1", ".ypy1-2", ".ypy1-3"], offsetX: 0, offsetY: 0, to: { x: 150, y: 180 } });
+	// rotateScale1(data)
+	// rotateScale2({list:[".ypy1-1", ".ypy1-2", ".ypy1-3"], offsetX:0, offsetY:0, to:{x:150, y:180}})
 
-	startBasic({ shiftY: 30 });
+	(0, _commonJsPlayer_supportJs.startBasic)({ shiftY: 30 });
 }
 
-var READ = {
-	t1: 2.2,
-	t2: 2
-};
-var tl = (0, _commonJsCommonJs.init)();
-var rotate = 150;
-tl.add("arcs-in");
-TweenLite.set(".ring", { opacity: 1 });
-var ring = { opacity: 0, ease: "power2.out", duration: .4 };
-tl.from(".ring1_1", _extends({}, ring, { rotate: -rotate }), "arcs-in+=0");
-tl.from(".ring1_2", _extends({}, ring, { rotate: rotate }), "arcs-in+=.1");
-tl.from(".ring1_3", _extends({}, ring, { rotate: -rotate }), "arcs-in+=.2");
-tl.from(".ring1_4", _extends({}, ring, { rotate: rotate }), "arcs-in+=.3");
-
-tl.from(".ypy1-1", { opacity: 0, rotate: -60, duration: .5 }, "arcs-in+=0.2");
-tl.from(".ypy1-2", { opacity: 0, rotate: -60, duration: .5 }, "arcs-in+=.5");
-tl.from(".ypy1-3", { opacity: 0, rotate: -60, duration: .5 }, "arcs-in+=.7");
-
-tl.add("shrink", "+=.3");
-
-tl.to(".ring", { opacity: 0, duration: .3 }, "shrink");
-tl.from(".ring_all", { opacity: 0, duration: .3 }, "shrink");
-
-tl.to(".hero", { x: 0, y: 0, scale: .50, duration: .3 }, "shrink");
-tl.from(".footer-bar", { y: _commonJsCommonJs.bannerSize.h, duration: .3 }, "shrink");
-tl.from(".t1", { y: 250, duration: .4 }, "shrink");
-tl.to(".ypy1-1", { x: -133, y: -178, duration: .3 }, "shrink");
-tl.to(".ypy1-2", { x: -173, y: -172, duration: .3 }, "shrink");
-tl.to(".ypy1-3", { x: -140, y: -191, duration: .3 }, "shrink");
-
-tl.to(".ring", { y: "-=" + 30, duration: .4 }, "shrink");
-
-tl.to(".t1", { opacity: 0, duration: .3 }, "+=" + READ.t1);
-tl.from([".t2", ".brand-logo"], { opacity: 0, duration: .3 });
-
-tl.add("end", "+=" + READ.t2);
-tl.to(".frame1", { y: "-=" + _commonJsCommonJs.bannerSize.h, duration: .5 }, "end");
-tl.set(".frame2", { opacity: 1 }, "end");
-tl.from(".frame2", { y: "+=" + _commonJsCommonJs.bannerSize.h, duration: .5 }, "end");
-
-tl.from(".url", { opacity: 0, duration: .3 });
-
-tl.add((0, _commonJsCommonJs.olg_ypy)(), "-=.3");
-
-// return tl
-
-},{"../../_common/js/common.js":1}]},{},[5])
+},{"../../_common/js/common.js":1,"../../_common/js/player_support.js":3}]},{},[6])
 
 
 //# sourceMappingURL=main.js.map
--- dev/safety_300x600/_bundled/main.js
+++ dev/safety_300x600/_bundled/main.js
@@ -99,9 +99,11 @@ Object.defineProperty(exports, "__esModule", {
 	value: true
 });
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var _commonJs = require('./common.js');
 
-document.getElementById("legalContent").innerHTML = "\n© 2023 IGT.<br/>\n© 2023 Evolution. All Rights Reserved.<br/>\n© 2023 Light &amp; Wonder, Inc. All Rights Reserved.<br/>\n<br/>\nMust be 19 years of age or older and a resident of Ontario, located in the province to play\nonline casino games. Games and screens may not appear as shown. Odds vary by game. Terms and conditions apply.\n<br/>\n*Voted most trusted Online Casino by Ontario shoppers based on the 2023 Brandspark® Canadian Trust Study.";
+document.getElementById("legalContent").innerHTML = "\n©2024 IGT.<br/><br/>\nMust be 19 years of age or older and a resident \nof Ontario, located in the province to play online\ncasino games. Games may not appear as shown. Odds vary by game. Terms and conditions apply.\n<br/>\n*Voted most trusted Online Casino by Ontario shoppers based on the 2023 Brandspark® \n<br/>\nCanadian Trust Study.";
 
 var READ = {
 	t1: 2.2,
@@ -112,23 +114,35 @@ function startBasic(_ref) {
 	var shiftY = _ref.shiftY;
 
 	var tl = (0, _commonJs.init)();
-	// return
-	var rotate = 180;
+	var rotate = 150;
 	tl.add("arcs-in");
-	tl.from(".ring1_1", { rotate: rotate, duration: .5 }, "arcs-in+=0");
-	tl.from(".ring1_2", { rotate: -rotate, duration: .8 }, "arcs-in+=0");
-	tl.from(".ring1_3", { rotate: rotate, duration: .6 }, "arcs-in+=0");
+	TweenLite.set(".ring", { opacity: 1 });
+	var ring = { opacity: 0, ease: "power2.out", duration: .4 };
+	tl.from(".ring1_1", _extends({}, ring, { rotate: -rotate }), "arcs-in+=0");
+	tl.from(".ring1_2", _extends({}, ring, { rotate: rotate }), "arcs-in+=.1");
+	tl.from(".ring1_3", _extends({}, ring, { rotate: -rotate }), "arcs-in+=.2");
+	tl.from(".ring1_4", _extends({}, ring, { rotate: rotate }), "arcs-in+=.3");
 
 	tl.from(".ypy1-1", { opacity: 0, rotate: -60, duration: .5 }, "arcs-in+=0.2");
 	tl.from(".ypy1-2", { opacity: 0, rotate: -60, duration: .5 }, "arcs-in+=.5");
 	tl.from(".ypy1-3", { opacity: 0, rotate: -60, duration: .5 }, "arcs-in+=.7");
 
 	tl.add("shrink", "+=.3");
+
+	tl.to(".ring", { opacity: 0, duration: .3 }, "shrink");
+	tl.from(".ring_all", { opacity: 0, duration: .3 }, "shrink");
+
 	tl.to(".hero", { x: 0, y: 0, scale: .50, duration: .3 }, "shrink");
 	tl.from(".footer-bar", { y: _commonJs.bannerSize.h, duration: .3 }, "shrink");
 	tl.from(".t1", { y: 250, duration: .4 }, "shrink");
+	tl.to(".ypy1-1", { x: -133, y: -178, duration: .3 }, "shrink");
+	tl.to(".ypy1-2", { x: -173, y: -172, duration: .3 }, "shrink");
+	tl.to(".ypy1-3", { x: -140, y: -191, duration: .3 }, "shrink");
 
-	tl.to(".ring", { y: "-=" + shiftY, duration: .4 }, "shrink");
+	tl.to(".ring", { y: "-=" + 30, duration: .4 }, "shrink");
+
+	tl.from([".inset"], { y: "+=" + 30, opacity: 0, duration: .4 }, "shrink+=.2");
+	tl.from([".brand-logo"], { opacity: 0, duration: .4 }, "shrink+=.2");
 
 	tl.to(".t1", { opacity: 0, duration: .3 }, "+=" + READ.t1);
 	tl.from([".t2", ".brand-logo"], { opacity: 0, duration: .3 });
